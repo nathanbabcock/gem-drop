9ad6dbc192dd81d083c714038c4bc14c37ccc1b2
QMCX63 <nathan.babcock@motorolasolutions.com>
2017-03-06 17:03:42 -0600

    Bar chart

--- idle.js
+++ idle.js
@@ -1,39 +1,89 @@
 // CONFIG
 var baserate = 1;
 var ticks_per_second = 100;
+var bar_maxheight = 100;
 
 var number = 0;
 var lastTime = null;
 //var time = 0;
-var upgrades = [
+var generators = [
 	{
-		name: "Upgrade A",
+		name: "Generator A",
 		//baseCost: 10,
 		owned: 0,
 		getCost: function() { return Math.floor(10 * Math.pow(1.3, this.owned)); },
 		getRate: function() { return 1; },
 		getTotalRate: function() { return this.getRate() * this.owned;}
-	}
+	},
+	{
+		name: "Generator B",
+		//baseCost: 10,
+		owned: 0,
+		getCost: function() { return Math.floor(100 * Math.pow(1.3, this.owned)); },
+		getRate: function() { return 10; },
+		getTotalRate: function() { return this.getRate() * this.owned;}
+	},
+	{
+		name: "Generator C",
+		//baseCost: 10,
+		owned: 0,
+		getCost: function() { return Math.floor(1000 * Math.pow(1.3, this.owned)); },
+		getRate: function() { return 100; },
+		getTotalRate: function() { return this.getRate() * this.owned;}
+	},
+	{
+		name: "Generator D",
+		//baseCost: 10,
+		owned: 0,
+		getCost: function() { return Math.floor(10000 * Math.pow(1.3, this.owned)); },
+		getRate: function() { return 1000; },
+		getTotalRate: function() { return this.getRate() * this.owned;}
+	},
 ];
 
 function init() {
+	// Construct generators HTML
+	for(var i = 0; i < generators.length; i++)
+		document.getElementById("generators").innerHTML += getGeneratorHTML(i);
+
 	setInterval(update, 1000 / ticks_per_second);
-	// for(var i = 0; i < upgrades.length; i++)
-	// 	updateUpgrade(i);
+	// for(var i = 0; i < generators.length; i++)
+	// 	updateGenerator(i);
+}
+
+function getGeneratorHTML(id){
+	return `
+		<div class="generator_container">
+			<div class="generator">
+				<strong class="generator_title" id="generator_`+id+`_name"></strong>
+				<br>Costs <span id="generator_`+id+`_cost"></span>
+				<br><span id="generator_`+id+`_owned"></span> owned
+				<br>+<span id="generator_`+id+`_rate"></span>/s
+				<br><button id="generator_`+id+`_button" onclick="buygenerator(`+id+`)">Buy</button>
+			</div>
+			<div class="bar" id="generator_`+id+`_bar"></div>
+		</div>`;
 }
 
 function update(){
 	var interval = getInterval;
 	number += getInterval() * getRate();
-	//number += (getRate()/ticks_per_second);
+
 	document.getElementById("number").innerHTML = formatNumber(number);
 	document.getElementById("rate").innerHTML = getRate();
-	for(var i = 0; i < upgrades.length; i++)
-		updateUpgrade(i);
+	document.getElementById("perclick").innerHTML = getPerClick();
+
+
+	for(var i = 0; i < generators.length; i++)
+		updateGenerator(i);
 }
 
 ////
 
+var perClick = 10;
+function getPerClick(){
+	return perClick;
+}
 
 function getInterval(){
 	var interval;
@@ -46,42 +96,60 @@ function getInterval(){
 	return interval / 1000;
 }
 
-function cheat(){
-	number += 10;
+function doClick(){
+	console.log("click");
+	number += getPerClick();
 }
 
-function updateUpgrade(id){
-	var upgrade = upgrades[id];
-	document.getElementById("upgrade_"+id+"_cost").innerHTML = upgrade.getCost();
-	document.getElementById("upgrade_"+id+"_rate").innerHTML = upgrade.getRate();
-	document.getElementById("upgrade_"+id+"_owned").innerHTML = upgrade.owned;
-	if(upgrade.getCost() > number){
-		document.getElementById("upgrade_"+id+"_cost").style.color="red";
-		document.getElementById("upgrade_"+id+"_button").disabled = true;
+function updateGenerator(id){
+	var generator = generators[id];
+
+	// Text info
+	document.getElementById("generator_"+id+"_name").innerHTML = generator.name;
+	document.getElementById("generator_"+id+"_cost").innerHTML = generator.getCost();
+	document.getElementById("generator_"+id+"_rate").innerHTML = generator.getRate();
+	document.getElementById("generator_"+id+"_owned").innerHTML = generator.owned;
+	document.getElementById("generator_"+id+"_owned").innerHTML = generator.owned;
+
+	// Bar
+	var percent = (generator.getTotalRate() / getRate());
+	var bar = document.getElementById("generator_"+id+"_bar")
+	bar.style.height = percent * bar_maxheight;
+	bar.innerHTML = Math.round(percent * 100) + "%";
+	if(percent <= 0.05)
+		bar.style.color = "black";
+	else
+		bar.style.color = "white";
+
+
+	// Enabled/disabled
+	if(generator.getCost() > number){
+		document.getElementById("generator_"+id+"_cost").style.color="red";
+		document.getElementById("generator_"+id+"_button").disabled = true;
 	}
 	else {
-		document.getElementById("upgrade_"+id+"_cost").style.color="green";
-		document.getElementById("upgrade_"+id+"_button").disabled = false;
+		document.getElementById("generator_"+id+"_cost").style.color="green";
+		document.getElementById("generator_"+id+"_button").disabled = false;
 	}
 }
 
-function buyUpgrade(id){
-	var upgrade = upgrades[id];
-	number -= upgrade.getCost();
-	upgrade.owned++;
-	//updateUpgrade(id);
+function buygenerator(id){
+	var generator = generators[id];
+	number -= generator.getCost();
+	generator.owned++;
+	//updateGenerator(id);
 }
 
-function getUpgrade(name){
-	for(var i = 0; i < upgrades.length; i++)
-		if(upgrades[i].name === name)
-			return upgrades[i];
+function getgenerator(name){
+	for(var i = 0; i < generators.length; i++)
+		if(generators[i].name === name)
+			return generators[i];
 }
 
 function getRate(){
 	var rate = baserate;
-	for(var i = 0; i < upgrades.length; i++)
-		rate += upgrades[i].getTotalRate();
+	for(var i = 0; i < generators.length; i++)
+		rate += generators[i].getTotalRate();
 	return rate;
 }
 
