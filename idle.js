ce0febda03a5267fe523e6787c9f35b4fbb6f328
QMCX63 <nathan.babcock@motorolasolutions.com>
2017-03-07 15:18:47 -0600

    Refactor DOM references

--- idle.js
+++ idle.js
@@ -12,9 +12,9 @@ var generators = [
 		name: "Generator A",
 		//baseCost: 10,
 		owned: 0,
-		getCost: function() { return Math.floor(10 * Math.pow(1.3, this.owned)); },
+		getCost: function(owned = this.owned) { return Math.floor(10 * Math.pow(1.3, owned)); },
 		getRate: function() { return 1; },
-		getTotalRate: function() { return this.getRate() * this.owned;}
+		getTotalRate: function(owned = this.owned) { return this.getRate(owned) * owned;}
 	},
 	{
 		name: "Generator B",
@@ -42,38 +42,92 @@ var generators = [
 	},
 ];
 
+function getGeneratorHTML(gen){
+	// TODO I wish I knew a less cancerous way of constructing DOM elements
+
+	// var str = `
+	// 	<div class="generator_container">
+	// 		<div class="generator">
+	// 			<strong class="name"></strong>
+	// 			<br>Costs <span class="cost"></span>
+	// 			<br><span class="owned"></span> owned
+	// 			<br>+<span class="rate"></span>/s
+	// 			<br><button class="button">Buy</button>
+	// 		</div>
+	// 		<div class="bar"></div>
+	// 		<div class="preview"></div>
+	// 	</div>`;
+	var refs = {};
+
+	// Container
+	refs.container = document.createElement("div");
+	refs.container.className = "generator_container";
+
+	// Generator
+	refs.generator = document.createElement("div");
+	refs.generator.className = "generator";
+
+	// Name
+	refs.name = document.createElement("strong");
+	refs.generator.appendChild(refs.name);
+
+	// Cost
+	refs.generator.appendChild(document.createElement("br"));
+	refs.generator.appendChild(document.createTextNode("Costs "));
+	refs.cost = document.createElement("span");
+	refs.generator.appendChild(refs.cost);
+
+	// Owned
+	refs.generator.appendChild(document.createElement("br"));
+	refs.owned = document.createElement("span");
+	refs.generator.appendChild(refs.owned);
+	refs.generator.appendChild(document.createTextNode(" owned"));
+
+	// Rate
+	refs.generator.appendChild(document.createElement("br"));
+	refs.generator.appendChild(document.createTextNode("+"));
+	refs.rate = document.createElement("span");
+	refs.generator.appendChild(refs.rate);
+	refs.generator.appendChild(document.createTextNode("/s"));
+
+	// Button
+	refs.generator.appendChild(document.createElement("br"));
+	refs.button = document.createElement("button");
+	refs.button.innerText = "Buy";
+	refs.button.onclick = function() { buyGenerator(gen); };
+	// refs.button.onmouseover = function() { previewPurchase(gen); };
+	// refs.button.onmouseout = function() { clearPreview(gen); };
+	// refs.button.ondeactivate = function() { clearPreview(gen); };
+	refs.generator.appendChild(refs.button);
+
+	refs.container.appendChild(refs.generator);
+
+	// Bar
+	refs.bar = document.createElement("div");
+	refs.bar.className = "bar";
+	refs.container.appendChild(refs.bar);
+
+	// Bar
+	refs.preview = document.createElement("div");
+	refs.preview.className = "preview";
+	refs.container.appendChild(refs.preview);
+
+	return refs;
+}
+
 function init() {
 	var genView = document.getElementById("generators");
 
 	// Construct generators HTML
 	for(var i = 0; i < generators.length; i++){
-		genView.innerHTML += getGeneratorHTML(i);
-		var thisGen = generators[i].container = genView.lastChild;
-		//thisGen.name = thisGen.getElementById("")
+		var refs = getGeneratorHTML(generators[i]);
+		genView.appendChild(refs.container);
+		generators[i].gui = refs;
 	}
 
 	setInterval(update, 1000 / ticks_per_second);
 	// for(var i = 0; i < generators.length; i++)
 	// 	updateGenerator(i);
-}
-
-function getGeneratorHTML(id){
-	return `
-		<div class="generator_container">
-			<div class="generator">
-				<strong class="generator_title" id="generator_`+id+`_name"></strong>
-				<br>Costs <span id="generator_`+id+`_cost"></span>
-				<br><span id="generator_`+id+`_owned"></span> owned
-				<br>+<span id="generator_`+id+`_rate"></span>/s
-				<br><button id="generator_`+id+`_button" 
-					onclick="buyGenerator(`+id+`)"
-					onmouseover="previewGenerator(`+id+`)"
-					onmouseout="clearPreview(`+id+`)"
-					ondeactive="clearPreview(`+id+`)">Buy</button>
-			</div>
-			<div class="bar" id="generator_`+id+`_bar"></div>
-			<div id="generator_`+id+`_preview"></div>
-		</div>`;
 }
 
 function update(){
@@ -85,20 +139,21 @@ function update(){
 	document.getElementById("perclick").innerHTML = getPerClick();
 
 	for(var i = 0; i < generators.length; i++)
-		updateGenerator(i);
+		updateGenerator(generators[i]);
 }
 
 ////
 
-function previewGenerator(id){
-	var preview = document.getElementById("generator_"+id+"_preview");
-	preview.style.backgroundColor="red";
-	preview.innerText="-x%";
+function previewPurchase(gen){
+	var preview = gen.gui.preview;
+	preview.style.backgroundColor="green";
+
+	preview.innerText="+x%";
 	preview.style.visibility = "visible";
 }
 
-function clearPreview(id){
-	var preview = document.getElementById("generator_"+id+"_preview");
+function clearPreview(gen){
+	var preview = gen.gui.preview;
 	preview.style.backgroundColor="red";
 	preview.innerText="-x%";
 	preview.style.visibility = "hidden";
@@ -125,23 +180,23 @@ function doClick(){
 	number += getPerClick();
 }
 
-function updateGenerator(id){
-	var generator = generators[id];
+function updateGenerator(gen){
+	//console.log("updating generator with name "+gen.name);
 
 	// Text info
-	document.getElementById("generator_"+id+"_name").innerHTML = generator.name;
-	document.getElementById("generator_"+id+"_cost").innerHTML = generator.getCost();
-	document.getElementById("generator_"+id+"_rate").innerHTML = generator.getRate();
-	document.getElementById("generator_"+id+"_owned").innerHTML = generator.owned;
-	document.getElementById("generator_"+id+"_owned").innerHTML = generator.owned;
+	gen.gui.name.innerHTML = gen.name;
+	gen.gui.cost.innerHTML = gen.getCost();
+	gen.gui.rate.innerHTML = gen.getRate();
+	gen.gui.owned.innerHTML = gen.owned;
+	gen.gui.owned.innerHTML = gen.owned;
 
 	// Bar
-	var percent = (generator.getTotalRate() / getRate());
+	var percent = (gen.getTotalRate() / getRate());
 	// var bar = d3.select("#generator_"+id+"_bar");//document.getElementById("generator_"+id+"_bar")
 	// bar.style("height", percent * bar_maxheight)
 	// 	.text(Math.round(percent * 100) + "%");
 
-	var bar = document.getElementById("generator_"+id+"_bar");//document.getElementById("generator_"+id+"_bar")
+	var bar = gen.gui.bar;//document.getElementById("generator_"+id+"_bar");//document.getElementById("generator_"+id+"_bar")
 	bar.style.height = percent * bar_maxheight;
 	bar.innerText=Math.round(percent * 100) + "%";
 
@@ -153,30 +208,29 @@ function updateGenerator(id){
 	else
 		bar.style.color = "white";
 
-
 	// Enabled/disabled
-	if(generator.getCost() > number){
-		document.getElementById("generator_"+id+"_cost").style.color="red";
-		document.getElementById("generator_"+id+"_button").disabled = true;
+	if(gen.getCost() > number){
+		gen.gui.cost.style.color = "red";
+		gen.gui.button.disabled = true;
+		clearPreview(gen);
 	}
 	else {
-		document.getElementById("generator_"+id+"_cost").style.color="green";
-		document.getElementById("generator_"+id+"_button").disabled = false;
+		gen.gui.cost.style.color = "green";
+		gen.gui.button.disabled = false;
 	}
 }
 
-function buyGenerator(id){
-	var generator = generators[id];
-	number -= generator.getCost();
-	generator.owned++;
+function buyGenerator(gen){
+	number -= gen.getCost();
+	gen.owned++;
 	//updateGenerator(id);
 }
 
-function getgenerator(name){
-	for(var i = 0; i < generators.length; i++)
-		if(generators[i].name === name)
-			return generators[i];
-}
+// function getgenerator(name){
+// 	for(var i = 0; i < generators.length; i++)
+// 		if(generators[i].name === name)
+// 			return generators[i];
+// }
 
 function getRate(){
 	var rate = baserate;
